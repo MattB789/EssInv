df990b558deb891aef07538907e8d8724e530e5d
MattB789 <mbainbr1@umbc,edu>
2025-05-06 03:59:41 -0400

    Made changes to the login system

--- login/main.js
+++ login/main.js
@@ -1,15 +1,5 @@
-import { initializeApp } from 'firebase/app';
-import {
-    getAuth,
-    signInWithPopup,
-    GoogleAuthProvider
-} from 'firebase/auth';
 
-// Import the functions you need from the SDKs you need
 
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -24,26 +14,35 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+
 
 // Initialize Firebase
+let id = 2; //admin = 1, student = 0, other = 2;
 
-const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
 
-const loginBtn = document.getElementById('loginBtn');
-const usernameDiv = document.getElementById('username');
 
-loginBtn.addEventListener('click', () => {
-    signInWithPopup(auth, provider)
-        .then(result => {
+function loginGoogle() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider)
+        .then((result) => {
             const user = result.user;
-            const name = user.displayName || "User";
-            usernameDiv.textContent = `Welcome, ${name}!`;
+            if (user.email === "sbearam1@gmail.com" || user.email === "mattmatt314159@gmail.com") {
+                id = 1;
+                console.log("You are", id);
+            }
+            else if (user.email === "sbearam1@umbc.edu" || user.email === "mbainbr1@umbc.edu") {
+                id = 0;
+                console.log("You are", id);
+            }
+            else {
+                id = 2;
+                console.log("You are", id);
+            }
         })
         .catch(error => {
             console.error('Error signing in:', error);
             alert("Login failed");
         });
-});
+}
