30504149b90309231dcf814a0b25d31d7bcec154
MattB789 <mbainbr1@umbc,edu>
2025-05-09 10:22:18 -0400

    Added more to the readme

--- login/README.md
+++ login/README.md
@@ -1,1 +1,4 @@
 You will need to install npm vita and npm install firebase.
+You will also need to install the regular frontend npm section.
+You will also need to use all the code.
+I had help from the firebase website, stack exchange, and other sources.
--- login/main.js
+++ login/main.js
@@ -1,38 +1,20 @@
 
 
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyDxlHZXw7gUdJlD8YTkjPFOy11ESx3p0N0",
-    authDomain: "essinv-347c3.firebaseapp.com",
-    projectId: "essinv-347c3",
-    storageBucket: "essinv-347c3.firebasestorage.app",
-    messagingSenderId: "341943484629",
-    appId: "1:341943484629:web:183c44f0f5f7408490e25c",
-    measurementId: "G-K24H73RWSS"
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-
-
 // Initialize Firebase
 let id = 2; //admin = 1, student = 0, other = 2;
 
 
-
+//logs into google and shows the user type
 function loginGoogle() {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    auth.signInWithPopup(provider)
+    const signin = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(signin)
         .then((result) => {
-            const user = result.user;
-            if (user.email === "sbearam1@gmail.com" || user.email === "mattmatt314159@gmail.com") {
+            const username = result.user;
+            if (username.email === "sbearam1@gmail.com" || username.email === "mattmatt314159@gmail.com") {
                 id = 1;
                 console.log("You are", id);
             }
-            else if (user.email === "sbearam1@umbc.edu" || user.email === "mbainbr1@umbc.edu") {
+            else if (username.email === "sbearam1@umbc.edu" || username.email === "mbainbr1@umbc.edu") {
                 id = 0;
                 console.log("You are", id);
             }
@@ -42,7 +24,9 @@ function loginGoogle() {
             }
         })
         .catch(error => {
-            console.error('Error signing in:', error);
-            alert("Login failed");
+            id = 2;
+            console.log("You failed to log in");
+
         });
 }
+
